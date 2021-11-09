fbba4e6c075bb6c83e3203f4a1cc23e3ccedd57c
Julia Mullen <julia.l.mullen@gmail.com>
2021-11-09 13:40:28 -0500

    update logger

--- src/outbreak_parser_tools/logger.py
+++ src/outbreak_parser_tools/logger.py
@@ -1,12 +1,14 @@
-try:
-    from biothings import config
-    logger = config.logger
-except ImportError:
-    import logging
-    logger = logging.getLogger('dataverse')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler('dataverse_log.log')
-    formatter = logging.Formatter('%(asctime)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+def get_logger(name):
+    try:
+        from biothings import config
+        logger = config.logger
+    except ImportError:
+        import logging
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(f'{name}_log.log')
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
+    return logger
