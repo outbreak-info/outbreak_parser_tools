615f79bac65bb4d754a85f7623e81a98c908cd4b
Julia Mullen <julia.l.mullen@gmail.com>
2022-01-11 18:55:58 -0500

    updated addendum code

--- src/outbreak_parser_tools/addendum.py
+++ src/outbreak_parser_tools/addendum.py
@@ -1,46 +1,86 @@
-import functools
+import os
 import json
 
-from collections import defaultdict
+HOME_DIR = '/opt/home/outbreak'
+HOME_DIR = '/Users/julia/code'
+ANNOTATION_PATHS = {
+    'topics_file':      os.path.join(HOME_DIR, 'topic_classifier',          'results', 'topicCats.json'),
+    'altmetrics_file':  os.path.join(HOME_DIR, 'covid_altmetrics',          'results', 'altmetric_annotations.json'),
+    'litcovid_updates': os.path.join(HOME_DIR, 'outbreak_preprint_matcher', 'results', 'update dumps', 'litcovid_update_file.json'),
+    'preprint_updates': os.path.join(HOME_DIR, 'outbreak_preprint_matcher', 'results', 'update dumps', 'preprint_update_file.json'),
+    'loe_annotations':  os.path.join(HOME_DIR, 'covid19_LST_annotations',   'results', 'loe_annotations.json')
+}
 
-CATS_FILE     = '/opt/home/outbreak/topic_classifier/results/topicCats.json'
-PREPRINT_FILE = '/opt/home/outbreak/outbreak_preprint_matcher/results/update dumps/update_file.json'
+class Annotation:
+    def __init__(self, source_file):
+        with open(source_file,'r') as infile:
+            self.annotations = json.load(infile)
 
-def add_addendum(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        addendums = get_addendums()
-        for document in func(*args, **kwargs):
-            document_with_addendums = addendums.get(document['_id'])
+    def relevant_annotations(self, documents):
+        d_ids = set([i['_id'] for i in documents])
+        return [i for i in self.annotations if i['_id'] in d_ids]
 
-            if document_with_addendums:
-                # prefers existing values for keys without overwriting anything
-                document_with_addendums.update(document)
-                yield document_with_addendums
+    def get_annotation(self, document, annotations):
+        return [a for a in annotations if a['_id']==document['_id']]
+
+class Correction(Annotation):
+    ## preprint matcher correction
+    def update(self, documents):
+        annotations = self.relevant_annotations(documents)
+
+        for document in documents:
+            correction = self.get_annotation(document, annotations)
+            if not correction:
+                continue
+
+            if document.get('correction'):
+                # not sure this branch is used at all
+                if isinstance(document['correction'], list):
+                    document['correction'].append(correction['correction'][0])
             else:
-                yield document
+                # document does not yet have a correction
+                document['correction'] = correction
+                print(f'{document["_id"]} correction {document["correction"]}')
 
-    return wrapper
+class Topic(Annotation):
+    def update(self, documents):
+        annotations = self.relevant_annotations(documents)
 
-def get_topic_categories(topic_categories_file, addendums):
-    with open(topic_categories_file, 'r') as categories_file:
-        categories_list = json.load(categories_file)
+        for document in documents:
+            topic = self.get_annotation(document, annotations)
+            if not topic:
+                continue
 
-    for category in categories_list:
-        addendums[category['_id']]['topicCategory'] = category['topicCategory']
-    return categories
+            topicslist = topic[0]['topicCategory'].replace("'","").strip("[").strip("]").split(",")
+            document['topicCategory'] = [x.strip(" ") for x in topicslist]
+            print(f"{document['_id']} topic {document['topicCategory']}")
 
-def get_preprint(preprint_file, addendums):
-    with open(preprint_file, 'r') as preprint_file:
-        preprint_list = json.load(preprint_file)
+class Metric(Annotation):
+    def update(self, documents):
+        annotations = self.relevant_annotations(documents)
 
-    for preprint in preprint_list:
-        addendums[preprint['_id']]['correction'] = preprint['correction']
-    return categories
+        for document in documents:
+            alt_info = self.get_annotation(document, annotations)
+            if not alt_info:
+                continue
+            alt_metric = alt_info[0]
 
-def get_addendums():
-    addendums = defaultdict(dict)
-    get_topic_categories(CATS_FILE, addendums)
-    get_preprint_match(PREPRINTS_FILE, addendums)
+            if document.get('evaluations'):
+                try:
+                    document['evaluations'].append(alt_metric['evaluations'][0])
+                except:
+                    eval_object = document['evaluations']
+                    document['evaluations']=[eval_object,alt_metric['evaluations'][0]]
+            else:
+                document['evaluations'] = alt_metric['evaluations']       
+                print(f'{document["_id"]} evaluation {document["evaluations"]}')
+                
+class Addendum:
+    def biorxiv_corrector(self):
+        return Correction(ANNOTATION_PATHS['preprint_updates'])
 
-    return dict(addendums)
+    def topic_adder(self):
+        return Topic(ANNOTATION_PATHS['topics_file'])
+
+    def altmetric_adder(self):
+        return Metric(ANNOTATION_PATHS['altmetrics_file'])
